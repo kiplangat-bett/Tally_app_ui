e9988a7236fd006e539c8149be00b776585c0dde
sgtbett <kiplangatsgt@gmail.com>
2021-06-22 09:40:31 +0300

    changed backend url

--- src/utils/http-common.js
+++ src/utils/http-common.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "https://usawagenda.herokuapp.com",
-  baseURL: "http://192.168.43.82:2222/",
+  baseURL: "https://usawagenda.herokuapp.com",
 });
